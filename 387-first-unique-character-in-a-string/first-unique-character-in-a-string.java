cd1c1edf8b321f72ca0b590afec6516e161c843c
Suresh Pattanaik <132614183+Spattanaik06@users.noreply.github.com>
2025-06-20 22:25:17 +0530

    Time: 79 ms (5.39%) | Memory: 45.5 MB (19.61%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -13,20 +13,22 @@ class Solution {
         // return -1;
 
 
-        Map<Character,Long> map=new LinkedHashMap<>();
-        for(char c : s.toCharArray())
-        {
-            Long count=map.getOrDefault(c,0L)+1;
-            map.put(c,count);
-        }
-        for(int i=0;i<s.length();i++)
-        {
-            if(map.get(s.charAt(i))==1)
-            {
-                return i;
-            }
-        }
+        // Map<Character,Long> map=new LinkedHashMap<>();
+        // for(char c : s.toCharArray())
+        // {
+        //     Long count=map.getOrDefault(c,0L)+1;
+        //     map.put(c,count);
+        // }
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(map.get(s.charAt(i))==1)
+        //     {
+        //         return i;
+        //     }
+        // }
         
-        return -1;
+        // return -1;
+
+       return s.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting())).entrySet().stream().filter(e->e.getValue()==1).map(Map.Entry::getKey).findFirst().map(s::indexOf).orElse(-1);
     }
 }
