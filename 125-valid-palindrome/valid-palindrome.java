1cb5e492184f7da0e61fbc668686f37507c9a0f4
Suresh Pattanaik <132614183+Spattanaik06@users.noreply.github.com>
2025-06-23 01:18:31 +0530

    Time: 15 ms (20.90%) | Memory: 45.7 MB (12.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,27 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) 
     {
-        //Using Built-in function
+        s= s.trim().replaceAll("[^A-Za-z0-9]","").toLowerCase();
 
-         s= s.trim().replaceAll("[^A-Za-z0-9]","").toLowerCase();
-    //     StringBuilder sb=new StringBuilder(onlyWord);
-    //     sb.reverse();
-    //     return (sb.toString().equals(onlyWord));
+    //Using Built-in function
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        return (sb.toString().equals(s));
 
-        //Without Built-In Function
-        int start=0;
-        int end=s.length()-1;
-        while(start<end)
-        {
-            if(s.charAt(start)!=s.charAt(end))
-            {
-                return false;
-            }
-            start++;
-            end--;
 
-        }
-        return true;
+
+
+    //Without Built-In Function
+        // int start=0;
+        // int end=s.length()-1;
+        // while(start<end)
+        // {
+        //     if(s.charAt(start)!=s.charAt(end))
+        //     {
+        //         return false;
+        //     }
+        //     start++;
+        //     end--;
+
+        // }
+        // return true;
         
     }
 }
