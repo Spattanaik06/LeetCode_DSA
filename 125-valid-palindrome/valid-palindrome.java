d72cca72025510c1766e0b110f4801794866ef69
Suresh Pattanaik <132614183+Spattanaik06@users.noreply.github.com>
2025-06-23 01:16:57 +0530

    Time: 15 ms (20.90%) | Memory: 44.6 MB (46.02%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,10 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) 
     {
-       String onlyWord= s.trim().replaceAll("[^A-Za-z0-9]","").toLowerCase();
-        StringBuilder sb=new StringBuilder(onlyWord);
-        sb.reverse();
-        return (sb.toString().equals(onlyWord));
+        //Using Built-in function
+
+         s= s.trim().replaceAll("[^A-Za-z0-9]","").toLowerCase();
+    //     StringBuilder sb=new StringBuilder(onlyWord);
+    //     sb.reverse();
+    //     return (sb.toString().equals(onlyWord));
+
+        //Without Built-In Function
+        int start=0;
+        int end=s.length()-1;
+        while(start<end)
+        {
+            if(s.charAt(start)!=s.charAt(end))
+            {
+                return false;
+            }
+            start++;
+            end--;
+
+        }
+        return true;
         
     }
 }
