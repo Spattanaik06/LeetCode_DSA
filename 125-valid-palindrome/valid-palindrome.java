587c0f116357280daaf15f64bfda856e635f341a
Suresh Pattanaik <132614183+Spattanaik06@users.noreply.github.com>
2026-03-01 01:21:33 +0530

    Time: 16 ms (13.31%) | Memory: 49.3 MB (5.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,28 +3,42 @@ class Solution {
     {
         s= s.trim().replaceAll("[^A-Za-z0-9]","").toLowerCase();
 
-    //Using Built-in function
-        StringBuilder sb=new StringBuilder(s);
-        sb.reverse();
-        return (sb.toString().equals(s));
+    // //Using stringBuilder
+    //     StringBuilder sb=new StringBuilder(s);
+    //     sb.reverse();
+    //     return (sb.toString().equals(s));
 
 
+    // //Using Two Pointer
+    //     int start=0;
+    //     int end=s.length()-1;
+    //     while(start<end)
+    //     {
+    //         if(s.charAt(start)!=s.charAt(end))
+    //         {
+    //             return false;
+    //         }
+    //         start++;
+    //         end--;
+
+    //     }
+    //     return true;
 
 
-    //Without Built-In Function
-        // int start=0;
-        // int end=s.length()-1;
-        // while(start<end)
-        // {
-        //     if(s.charAt(start)!=s.charAt(end))
-        //     {
-        //         return false;
-        //     }
-        //     start++;
-        //     end--;
-
-        // }
-        // return true;
-        
+        //recursion
+        int start=0;
+        int end=s.length() -1;
+      boolean b=print(s,start,end);  
+      return b;
     }
+    public static boolean print(String s,int start,int end)
+    {
+        if(start>=end) return true;
+        if(s.charAt(start)!=s.charAt(end))
+        {
+            return false;
+        }
+       return print(s,start +1,end -1);
+    }
+
 }
